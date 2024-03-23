2e6139db00624a82e0215a672d3a0ee572247dfa
karungop <127687320+karungop@users.noreply.github.com>
2024-03-23 17:43:59 -0400

    Update urls.py

--- Maestro/urls.py
+++ Maestro/urls.py
@@ -17,15 +17,15 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-from BookWorm.views import home_view
-from StudentWorm.views import student_articles
-from TeacherWorm.views import add_article
+from Maestro.views import home_view
+from Study.views import student_articles
+from Questions.views import add_article
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path('teacher-worm/', include('TeacherWorm.urls')),
-    path('teacher-worm/', add_article, name='add_articles'),
-    path('student-worm/', student_articles, name='student_articles'),
-    path('student-worm/', include('StudentWorm.urls')),
+    path('questions/', include('Questions.urls')),
+    path('questions/', add_article, name='add_articles'),
+    path('study/', student_articles, name='student_articles'),
+    path('study/', include('Study.urls')),
     path('', home_view, name='home'),  # Map the root URL to the home_view
 ]
